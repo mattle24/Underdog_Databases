c11c1eb7f7397000ed26800fb8dc317d789dec1d
Chris Sciavolino <cds253@cornell.edu>
2017-09-09 15:02:07 -0400

    Redesigned home page with new css styles and coding conventions. Used php includes statement for the navbar. Changed how the paragraphs of text looks on larger screens

---
+++ includes/navbar.php
@@ -0,0 +1,25 @@
+<?php
+echo '
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.php">Grassroots Analytics</a>
+                    <button class="navbar-toggle callapsed"
+                            data-toggle="collapse"
+                            data-target="#collapsemenu">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div> <!-- end navbar-header -->
+
+                <div id="collapsemenu" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="centered"><a href="index.php" class="nav-ele">Home</a></li>
+                        <li class="centered"><a href="about.php" class="nav-ele">Login</a></li>
+                    </ul>
+                </div> <!-- end collapsemenu -->
+            </div> <!-- end container -->
+        </nav>
+';
+?>
---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Grassroots Analytics</title>
+        <!-- Source Sans Pro font -->
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+        <link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+        <link rel='stylesheet' type='text/css' href="styles/all.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
+    </head>
+    <body>
+
+        <?php include 'includes/navbar.php' ?>
+
+        <div id='home-banner'>
+            <img id='home-hero-image' src='images/background.png' alt='Blue City Line Background'>
+            <div id='home-hero-wrapper'>
+                <div id='home-hero-title'>
+                    <span>Grassroots Analytics</span>
+                </div>
+                <div id='home-hero-subtitle'>
+                    <span>Bringing Big Data to Small Campaigns</span>
+                </div>
+                <div id='home-hero-buttons'>
+                    <a class='cta-button'>Sign up</a>
+                    <a class='cta-button'>Login</a>
+                </div>
+            </div>
+        </div>
+
+        <div id='home-container' class='container'>
+            <div class='row'>
+                <article class='home-text-block col-md-4'>
+                    <h3 class='block-header page-header'>Why Grassroots Analytics</h3>
+                    <p>
+                        Grassroots Analytics is a platform built for small campaigns. We started Grassroots after being able access an updated voter file before the 2016 election. Our goal is to create a platform so that any progressive campaign, no matter the size, can use data to inform their positions. [Should talk about why we value data]
+                    </p>
+                </article>
+
+                <article class='home-text-block col-md-4'>
+                    <h3 class='block-header page-header'>The Importance of Data</h3>
+                    <p>
+                        Whether you are a city council or presidential campaign, you want to maximize your voter contact efforts. Time spent on voters that do not support your candidate is time you cannot spend talking to your potential supporters. By using a data-driven approach you can maximize your efforts and win crucial votes.
+                    </p>
+                </article>
+
+                <article class='home-text-block col-md-4'>
+                    <h3 class='block-header page-header'>How to Sign Up</h3>
+                    <p>
+                        We created GrassRoots Analytics as a platform that any progressive campaign or group could use. The platform is free to use, although donations keep it running. To begin using the Grassroots platform, contact us. We'll be in touch shortly about how we can help you bring a data-driven approach to your campaign.
+                    </p>
+                </article>
+            </div>
+        </div> <!-- end container -->
+        
+        <footer>
+        </footer>
+
+    </body>
+</html>
