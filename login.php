b09d201bc8d4c05b86e817d85d3045b9d57d9c6b
Chris Sciavolino <cds253@cornell.edu>
2017-10-28 17:23:45 -0400

    Added a template for login.php that you can look off of for other pages.

--- login.php
+++ login.php
@@ -1,66 +1,73 @@
-<?php session_start();?>
+<?php 
 
-<!DOCTYPE html>
-<html>
-<head>
-   <title>Login</title>
-   <!-- Source Sans Pro font -->
-   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
-   <link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
-   <link rel='stylesheet' type='text/css' href="styles/all.css">
-   <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-   <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
-</head>
-<body>
-  <?php include 'includes/navbar.php' ?>
-  <div id = 'page-header1'>
-  <div class = 'spacer'></div>
-  <div id = 'my-form'>
-        <?php 
+session_start(); 
+$err_msg = "";
+if ( isset($_SESSION['logged_user']) ) {
+    // User is already logged in, redirect
+    header("Location: choose_campaign.php");
+}
+if ( isset($_POST['submit']) ) {
+    // submit button pressed, evaluate the form
+    if ( isset($_POST['username']) && isset($_POST['password']) ) {
+        // Both fields were filled in, evaluate them
         $post_username = filter_input( INPUT_POST, 'username', FILTER_SANITIZE_STRING );
         $post_password = filter_input( INPUT_POST, 'password', FILTER_SANITIZE_STRING );
-        if (empty($post_username) || empty($post_password)) { 
-          ?>
-          <form action = 'login.php' method = 'post' id = 'login'>
-              <label>Email</label>
-              <input type = 'text' name = 'username'required> <br><br>
-              <label>Password</label>
-              <input type = 'password' name = 'password'> <br> <br>
-              <button type = 'submit' value = 'Submit' formid = 'login'>Login</button>
-          </form>
-          <button>Forgot Password?</button>
-          <!-- TODO: add a forgot password action series -->
-          <a href = 'new_user.php'><button >New Account</button></a>
-          <?php
-      } else {
-        include("configs/config.php");
-        $db = new mysqli(
-          DB_HOST, 
-          DB_USER, 
-          DB_PASSWORD, 
-          DB_NAME
-          )or die('Failed to connect.'); 
+
+        require_once("configs/config.php");
+        $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('Failed to conn.');
         $query = "SELECT email, hashpassword, first FROM users WHERE email = ?;";
-        $stmt = $db->prepare($query);
+        $stmt = $conn->prepare($query);
         $stmt->bind_param('s', $post_username);
         $stmt->execute();
         $stmt->store_result();
         $stmt->bind_result($email, $hashpassword, $first);
         $stmt->fetch();
-        if ($stmt->num_rows == 1 && password_verify($post_password, $hashpassword)) {
-          echo "<p>You have logged in $first!</p>";
-          $_SESSION['logged_user'] = $post_username;
-          header('Location: choose_campaign.php');
-      }
-      else {
-          echo '<br><p>You did not login successfully.</p>';
-          echo '<p>Please <a href="login.php">login</a></p>';
-      }
-  }
-  ?>
-</div>
-</div>
-<footer>
-</footer>
-</body>
+                        
+        if ( $stmt->num_rows === 1 && password_verify($post_password, $hashpassword) ) {
+            // Successful login, set logged_user in $_SESSION and redirect
+            $_SESSION['logged_user'] = $post_username;
+            header('Location: choose_campaign.php');
+        } else {
+            // Invalid username or password
+            $err_msg = "Invalid username or password.";
+        }
+    } else {
+        // Required fields were not filled out properly
+        $err_msg = "Please fill out all required fields below.";
+    }
+} 
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <!-- Source Sans Pro font -->
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+        <link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+        <link rel='stylesheet' type='text/css' href="styles/all.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
+    </head>
+    <body>
+        <?php include 'includes/navbar.php' ?>
+        <div id = 'page-header1'>
+            <div class='spacer'></div>
+            <div id='my-form'>
+                <form action='login.php' method='post' id='login'>
+                    <?php echo "<p>$err_msg</p>"; ?>
+                    <label>Email</label>
+                    <input type = 'text' name = 'username'required> <br><br>
+                    <label>Password</label>
+                    <input type = 'password' name = 'password'> <br> <br>
+                    <button type = 'submit' name='submit' value = 'Submit' formid = 'login'>Login</button>
+                </form>
+                <button>Forgot Password?</button>
+                <!-- TODO: add a forgot password action series -->
+                <a href = 'new_user.php'><button>New Account</button></a>
+            </div>
+        </div>
+        <footer>
+        </footer>
+    </body>
 </html>
